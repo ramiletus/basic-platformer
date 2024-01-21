670c4c0451a3ee14ca370222866a8bf6fce40fe1
DESKTOP-33ONTHS\Juan Ramil Díaz <juan.ramil@udc.es>
2024-01-21 12:18:18 +0100

    Minimun side scrolling, the platform moves as if it were the background

--- index.js
+++ index.js
@@ -44,7 +44,29 @@ class Player {
     }
 }
 
-const player = new Player(100, 100)
+class Platform {
+    constructor(widht, height) {
+        this.position = {
+            x: 200,
+            y: 150
+        }
+        
+        this.width = widht
+        this.height = height
+    }
+
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+
+}
+
+const player = new Player(100, 200)
+
+const platform = new Platform(150, 25)
+
 const keys = {
     right: {
         pressed: false
@@ -53,20 +75,41 @@ const keys = {
         pressed: false
     }
 }
+
 player.draw()
 
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+    platform.draw()
 
-    if (keys.right.pressed) {
-        player.velocity.xVelocity = 5;
-    } else if (keys.left.pressed) {
-        player.velocity.xVelocity = -5;
+    if (keys.right.pressed && player.position.x < 400) {
+        player.velocity.xVelocity = 3;
+    } else if (keys.left.pressed && player.position.x > 100) {
+        player.velocity.xVelocity = -3;
     } else {
         player.velocity.xVelocity = 0;
+
+        if (keys.right.pressed) {
+            platform.position.x -= 3
+        } else if (keys.left.pressed) {
+            platform.position.x += 3
+        }
     }
+
+    //collision detection with a platform
+    if (
+            player.position.y + player.height <= platform.position.y 
+        &&  player.position.y + player.height + player.velocity.yVelocity >= platform.position.y
+        &&  player.position.x + player.width >= platform.position.x
+        &&  player.position.x + player.width <= platform.position.x + platform.width 
+        )
+    {
+        player.velocity.yVelocity = 0;
+    }
+    
+
 
 }
 
@@ -91,7 +134,7 @@ addEventListener('keydown', ({code}) => {
             
         case 'KeyW':
             console.log('up')
-            player.velocity.yVelocity -= 20
+            player.velocity.yVelocity -= 50
             break;
     
         default:
